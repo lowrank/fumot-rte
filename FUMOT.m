18601de7693aa0a36118feb1c8ef2b933ca05e6f
Yimin Zhong <yzhong@utexas.edu>
2019-01-22 22:53:44 -0600

    Updated submodule rte

--- FUMOT.m
+++ FUMOT.m
@@ -6,6 +6,10 @@ classdef FUMOT < handle
         sigma
         gamma
         boundary
+        H
+        model
+        
+        beta
     end
     
     methods (Access = public)
@@ -14,53 +18,113 @@ classdef FUMOT < handle
             
             if ~(isfield(opt, 'sigma') )
                 error('Reference to non-existing field:FUMOT');
+            else
+                % for the first stage only.
+                % function handles in opt. OUTPUT rows vector.
+                obj.sigma.xs = opt.sigma.xs(obj.RTE.nodes);
+                obj.sigma.xa = opt.sigma.xa(obj.RTE.nodes);
+                obj.sigma.xf = opt.sigma.xf(obj.RTE.nodes);
             end
             
-            if ~(isfield(opt, 'gamma') )
-                error('Reference to non-existing field:FUMOT');
+            if ~(isfield(opt, 'gamma'))
+                obj.gamma.x = 1;
+                obj.gamma.f = 1;
+            else
+                % constants, default values are 1.
+                obj.gamma.x  = opt.gamma.x;
+                obj.gamma.f  = opt.gamma.f;  
             end
+           
             
             if ~(isfield(opt, 'boundary') )
                 error('Reference to non-existing field:FUMOT');
+            else
+                obj.boundary = opt.boundary; 
             end            
-           
             
-            % for the first stage only.
-            % function handles in opt. OUTPUT rows vector.
-            obj.sigma.xs = opt.sigma.xs(obj.RTE.nodes);
-            obj.sigma.xa = opt.sigma.xa(obj.RTE.nodes);
-            obj.sigma.xf = opt.sigma.xf(obj.RTE.nodes);
-
-            obj.boundary = opt.boundary; 
+            obj.beta = 1e-3;
             
-            % constants
-            obj.gamma.x  = opt.gamma.x;
-            obj.gamma.f  = opt.gamma.f;
+            [obj.H, ~] = obj.ExciteForwardOp(obj.sigma.xf);
+            
             
         end
         
-        function [H, psi] = ExciteForwardOp(obj, XF)
+        function S = ScatterOp(obj, X)
+            rX = reshape(X, obj.RTE.nAngle, obj.RTE.nPoint);
+            S  = ifft(bsxfun(@times, obj.RTE.pFHG', fft(rX))) * obj.RTE.dtheta;  
+        end
+        
+        function resetBoundaryCondition(obj)
+            obj.RTE.boundarySource = zeros(obj.RTE.nAngle, obj.RTE.nPoint);
+        end
+        
+        function [H, Up, psi] = ExciteForwardOp(obj, XF)    
             obj.RTE.setBoundaryCondition(obj.boundary);
-            obj.RTE.sigmaT = obj.sigma.xa + XF + obj.sigma.xs; ...
-            obj.RTE.sigmaS = obj.sigma.xs;
-            
+            sigmaXTF = obj.sigma.xa + XF + obj.sigma.xs; ...
+            sigmaS   = obj.sigma.xs;
+
+            obj.RTE.sigmaT = sigmaXTF;
+            obj.RTE.sigmaS = sigmaS;
+
             nAngle = obj.RTE.nAngle;
-            
+
             % The rte routine gives the adjoint solution.
             % U_{-} = u(x, -v);
-            Un = obj.RTE.ForwardSolve(); 
-            
             % Reverse the directions by flipping the bottom half to top.
             % U_{+} = u(x, v) as the exact solution.
+
+            Un = obj.RTE.ForwardSolve(); 
             Up = [Un(nAngle/2+1:end, :) ; Un(1:nAngle/2, :)]; 
-            psi = sum(Un .* Up , 1) / nAngle;
-            
-            H = obj.gamma.f * XF .* psi;
-            
+            psi = sum(Un .* Up               , 1)/ nAngle;           
+            phi = sum(Un .* obj.ScatterOp(Up), 1)/nAngle;
+
+            H = -obj.gamma.f * sigmaXTF .* psi + obj.gamma.x * sigmaS .* phi;
         end
        
+        function [f, g] = ExciteBackwardOp(obj, XF)
+            [H0, Up, psi] = ExciteForwardOp(obj, XF);
+            mismatch = H0 - obj.H; % row vec
+            f = 0.5 * mismatch * obj.RTE.M * mismatch' + 0.5 * obj.beta * XF * obj.RTE.S * XF';
+            g = obj.beta * obj.RTE.S * XF';
+            
+            % first part in gradient
+            g = g - obj.RTE.M * (mismatch .* psi)';
+            
+            % solves adjoint equation with internal source
+            sigmaXTF = obj.sigma.xa + XF + obj.sigma.xs; ...
+            sigmaS   = obj.sigma.xs;
         
-        % do a stochatic gradient?
+            % the transpose is to make sure output as angle x space.
+            Q  = - bsxfun(@times, 2 * sigmaXTF', Up')' + ...
+                bsxfun(@times, 2 * sigmaS', (obj.ScatterOp(Up))' )';
+            
+            Z = -bsxfun(@times, mismatch', Q')';
+            Y = reshape(  obj.RTE.rays.interior_transport(...
+                obj.RTE.nodes, obj.RTE.elems, obj.RTE.sigmaT,  Z), obj.RTE.nAngle * obj.RTE.nPoint, 1);
+            % solve the transport equation
+            forwardMap = @(X) (X - obj.RTE.mapping(reshape(X, obj.RTE.nAngle, obj.RTE.nPoint))  );
+            [Xp, ~] = gmres(forwardMap, Y, 10, 1e-10, 400); 
+            
+            Xp = reshape(Xp, obj.RTE.nAngle, obj.RTE.nPoint);
+            Xn = [Xp(obj.RTE.nAngle/2+1:end, :) ; Xp(1:obj.RTE.nAngle/2, :)]; 
+            
+            T = sum(Up .* Xn               , 1)/ obj.RTE.nAngle; 
+            g = g + obj.RTE.M * T';
+  
+        end
+        
+        function [xf,fval,exitflag,output]  = ExciteSolve(obj, XF)
+            
+            options = optimset('Diagnostics','on','DerivativeCheck','off',...
+                'FinDiffType','central','LargeScale','off',...
+                'GradObj','on','Display','iter-detailed',...
+                'TolFun',1e-9,'TolX',1e-6,'MaxFunEvals',800,....
+                'MaxIter',500,'HessUpdate','bfgs');
+          
+            [xf,fval,exitflag,output] = fminunc(@obj.ExciteBackwardOp, XF,options);
+            
+        end
+
         
     end
     
--- test.m
+++ test.m
@@ -16,51 +16,29 @@
 
 clear;clc;
 
-boundary = @(x,y,v) (1+cos(4*pi*x)*cos(4*pi*y));
+boundary = @(x,y,v) (1);%sin(3 * pi * y)^2 + sin(3 * pi * x)^2);
 
 sigma = struct(...
-    'xs', @(x) (2 + 0.2 * x(1, :)),...
-    'xa', @(x) (0.2 + 0.02 * x(2, :)) , ...
-    'xf', @(x) (1.0 + 0.5 * x(1, :)));
+    'xs', @(x) (10 + 0.2 * x(1, :)),...
+    'xa', @(x) (0.2 + 0.2 * x(2, :)) , ...
+    'xf', @(x) (0.5 + 0.5 * x(1, :)));
 gamma = struct('x', 1, 'f', 1);
 
-opt = struct('anisotropy', 0.5, 'angle', 16, ...
-    'nodes', [0 0; 1 0; 1 1; 0 1]', 'minArea', 0.0001,...
+% opt must have even number of angles in the angular space.
+% used for perfect flipping. For mild anisotrpy, a relative small number
+% suffices.
+opt = struct('anisotropy', 0.5, 'angle', 32, ...
+    'nodes', [0 0; 1 0; 1 1; 0 1]', 'minArea', 1e-4,...
     'sigma', sigma, 'boundary', boundary, 'gamma', gamma);
 
 % construct FUMOT instance.
 ft = FUMOT(opt);
 
-% initialize fluorescent coefficient at excitation wavelength.
+
+%% initialize fluorescent coefficient at excitation wavelength.
 XF =  zeros(1, ft.RTE.nPoint); % row vec.
-
-% internal data from excitation stage.
-
-[H, psi] = ft.ExciteForwardOp(ft.sigma.xf);
-
-% induction step 1.
-[~, psi0] =  ft.ExciteForwardOp(XF);
-
-
-% inductions
-for i = 1:10
-    XF = H ./(opt.gamma.f * psi0);
-    [H0, psi0] = ft.ExciteForwardOp(XF);
-    disp(norm(H - H0));
-    if ~all(H > H0)
-        error('scheme not monotone.');
-    end
-        
-    
-end
-
 %%
-figure(1);
-ft.RTE.plot(XF');
-
-figure(2);
-ft.RTE.plot(ft.sigma.xf');
-
+[xf,fval,exitflag,output] = ft.ExciteSolve(XF);
 
 
 
